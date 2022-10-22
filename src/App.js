d08e3f377aaec14516d8b08ed6427a5c0c5f6805
agu415 <agustin415@gmail.com>
2022-10-21 21:18:42 -0300

    integracion react MUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const element = <h1>Hello, Agustin!</h1>;
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        {element}
       </header>
     </div>
   );
