bed256385ecf42e04bd1e2646ac06b0d4edd72fc
agu415 <agustin415@gmail.com>
2022-10-22 22:25:06 -0300

    finalizado

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,16 +2,31 @@ import React, {useEffect} from "react";
 import {
     Grid,
     Paper,
-    Box
+    Box,
+    Link,
+    Typography,
+    Breadcrumbs,
+    
 }  from '@mui/material';
+import Todo from "../todo/Todo";
+import FetchList from "../fetchList/FetchList";
+
 const Dashboard = () => {
     return (
-        <Grid container spacing={3}>
+        <Grid container spacing={3} >
             <Grid item xs={12}>
-                <Paper sx={{p: 2}}>
-                    <Box>
-                        Dashboard
-                    </Box>
+                <Paper sx={{p: 2}} >
+                <Breadcrumbs aria-label="breadcrumb" >
+                    <Link color="inherit" href="" onClick={Dashboard}>
+                        HOME
+                    </Link>
+                    <Link color="inherit" href="todo" onClick={Todo}>
+                        TODO
+                    </Link>
+                    <Link color="inherit" href="fetch-list" onClick={FetchList}>
+                        FECTH-LIST
+                    </Link>
+                </Breadcrumbs>
                 </Paper>
             </Grid>
         </Grid>
